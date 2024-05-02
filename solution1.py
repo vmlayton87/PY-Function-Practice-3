b9752102fac2fa04973fce2ee824fef5a3026add
vmlayton87 <vmlayton87@gmail.com>
2024-05-02 18:29:05 -0400

    solution 1

--- solution1.py
+++ solution1.py
@@ -1,1 +1,7 @@
 # name_args - Accepts any number of named arguments and prints them in the pattern key : value one at a time
+
+def name_args(**kwargs):
+    for i in kwargs:
+        print(i, ':', kwargs[i])
+
+name_args(a='abc', b='def', c='ghi')
